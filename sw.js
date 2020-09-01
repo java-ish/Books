f4279a359cae98c6c9a8c211dade962802a7c0f0
java-ish <manish.lp3000@gmail.com>
2020-09-01 14:43:15 +0545

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,41 @@
-// On install - caching the application shell
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add('index.html');
-    })
-  );
+const cacheName = "BFM-v1";
+const staticAssets = ["./", "./index.html", "./css/style.css"];
+
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
 });
 
-// On network request
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
 });
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
